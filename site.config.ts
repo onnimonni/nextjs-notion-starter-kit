cd88f24401ac075c473f8c42f503a41c95ad7903
Onni Hakala <onni@keksi.io>
2024-02-07 18:03:30 +0100

    Replace travelingvanproblem.com with flaky.build

--- site.config.ts
+++ site.config.ts
@@ -2,29 +2,29 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '708bfd41fda24b989e3f0788dc603f82',
+  rootNotionPageId: '913af13d078d4ccbbd3fe16afc0ea17f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Traveling Van Problem',
-  domain: 'travelingvanproblem.com',
-  author: 'Traveling Van Problem',
+  name: 'Flaky Build',
+  domain: 'flaky.build',
+  author: 'Onni Hakala',
 
   // open graph metadata (optional)
-  description: 'Blog & Guidebook about traveling with a camper van in Europe 🚚 🇪🇺',
+  description: 'Hacky software development and engineering practices 💻🔥',
 
   // social usernames (optional)
   //twitter: 'vanproblem',
-  github: 'travelingvanproblem',
+  github: 'flakybuild',
   //linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  instagram: 'travelingvanproblem', // optional instagram username
-  youtube: 'TravelingVanProblem', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-  reddit: 'TravelingVanProblem', // optional reddit username
+  instagram: 'flaky.build', // optional instagram username
+  // youtube: 'TravelingVanProblem', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  // reddit: 'TravelingVanProblem', // optional reddit username
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
