c421a765c2081e62e2dc44d6c5599efe49b42dad
Onni Hakala <onni@flaky.build>
2024-10-29 21:53:59 +0200

    Improve Machine readable version of the site by adding JSON-LD document to each page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,120 @@ export default class MyDocument extends Document {
             {/* Remember to keep the key="" attributes here if you change favicon and theme colors */}
             <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fefffe" key="theme-color-light"/>
             <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#2d3439" key="theme-color-dark"/>
+
+            {/* Try to get better Google visibility by adding the machine readable version here */}
+            {(
+              <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                  __html: JSON.stringify({
+                    "@context": "https://schema.org",
+                    "@graph": [
+                      {
+                        "@type": "WebPage",
+                        "@id": "https://keksi.io/",
+                        "url": "https://keksi.io/",
+                        "name": "Tukiopetusta etänä erityisopetuksen asiantuntijalta",
+                        "isPartOf": { "@id": "https://keksi.io/#website" },
+                        "about": { "@id": "https://keksi.io/#organization" },
+                        "datePublished": "2023-12-16T21:29:41+00:00",
+                        "dateModified": "2024-10-29T13:02:01+00:00",
+                        "description": "Yksilöllistettyä opetusta erityispedagogiikan ammattilaiselta - Keksi Labs Oy",
+                        "breadcrumb": { "@id": "https://keksi.io/#breadcrumb" },
+                        "inLanguage": "fi",
+                        "potentialAction": [
+                          { "@type": "ReadAction", "target": ["https://keksi.io/"] }
+                        ]
+                      },
+                      {
+                        "@type": "BreadcrumbList",
+                        "@id": "https://keksi.io/#breadcrumb",
+                        "itemListElement": [
+                          { "@type": "ListItem", "position": 1, "name": "Etusivu" }
+                        ]
+                      },
+                      {
+                        "@type": "WebSite",
+                        "@id": "https://keksi.io/#website",
+                        "url": "https://keksi.io/",
+                        "name": "Keksi Labs",
+                        "description": "Yksilöllistä tukiopetusta etänä",
+                        "publisher": { "@id": "https://keksi.io/#organization" },
+                        "inLanguage": "fi"
+                      },
+                      {
+                        "@type": "Organization",
+                        "@id": "https://keksi.io/#organization",
+                        "name": "Keksi Labs",
+                        "url": "https://keksi.io/",
+                        "logo": "https://keksi.io/favicon.svg",
+                        "email": "meri@keksi.io",
+                        "telephone": "+47-99-999-9999",
+                        "address": {
+                          "@type": "PostalAddress",
+                          "addressLocality": "Tampere",
+                          "addressCountry": "FI",
+                          "addressRegion": "Pirkanmaa"
+                        },
+                        "vatID": "FI27345244",
+                        "sameAs": [
+                          "https://www.instagram.com/keksi_labs",
+                          "https://www.linkedin.com/company/keksilabs"
+                        ]
+                      },
+                      {
+                          "@type": "Product",
+                          "@id": "https://keksi.io/#product",
+                          "name": "Tukiopetusta etänä",
+                          "image": "https://keksi.io/favicon.svg",
+                          "description": "Tukiopetusta etänä erityisopetuksen asiantuntijalta suomen kielellä Google Meet -alustalla",
+                          "keywords": "Tukiopetus, Erityisopettaja, Etäopetus, Suomi, Peruskoulu, Yläaste, Matematiikka, Äidinkieli",
+                          "sku": "",
+                          "brand": {
+                              "@type": "Brand",
+                              "name": "Keksi Labs"
+                          },
+                          "offers": {
+                              "@type": "Offer",
+                              "url": "https://keksi.io/ajanvaraus",
+                              "priceCurrency": "EUR",
+                              "price": "50",
+                              "availability": "https://schema.org/InStock",
+                              "seller": {
+                                  "@type": "Organization",
+                                  "@id": "https://keksi.io/#organization"
+                              }
+                          },
+                          "review": [{
+                            "@type": "Review",
+                            "datePublished": "2024-09-02",
+                            "name": "Ei tarvinnut jäädä yksin läksyjen kanssa",
+                            "reviewBody": "Motivaatio oli nuorella vähän hakusessa ja yhdessä Merin kanssa hän löysi ihan uuden vaihteen koulutöiden tekemiseen ja ovet mieluisaan lukioon aukesivat. ",
+                            "reviewRating": {
+                              "@type": "Rating",
+                              "ratingValue": 5
+                            },
+                            "author": {
+                              "@type": "Person",
+                              "name": "Johanna Kivistö",
+                              "sameAs": [
+                                "https://www.linkedin.com/in/johanna-kivist%C3%B6-she-her-90b0942/"
+                              ]
+                            }
+                          }],
+                          "aggregateRating": {
+                              "@type": "AggregateRating",
+                              "bestRating": 5,
+                              "worstRating": 1,
+                              "ratingCount": 3,
+                              "ratingValue": 5
+                          }
+                      }
+                    ]
+                  }),
+                }}
+              />
+            )}
           </Head>
 
           <body>
