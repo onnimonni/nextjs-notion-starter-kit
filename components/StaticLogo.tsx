dffcdef5e7975e9fe3b3ab4bb60defcc1edba22a
Onni Hakala <onni@flaky.build>
2024-11-04 21:34:16 +0200

    Enable flaky.build icons and settings

--- components/StaticLogo.tsx
+++ components/StaticLogo.tsx
@@ -6,7 +6,7 @@ import styles from './StaticLogo.module.css'
 export function StaticLogo() {
   return (
     <a className={cs(styles.logo, styles.link)} href={config.host} rel="home" title="Logo">
-      <img src="/favicon.png"/>
+      <img src="/favicon.svg"/>
       <span>{config.name}</span>
     </a>
   )
--- site.config.ts
+++ site.config.ts
@@ -2,35 +2,29 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '913af13d078d4ccbbd3fe16afc0ea17f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Flaky Build',
+  domain: 'flaky.build',
+  author: 'Onni Hakala',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: "Undeterministic blog on product development and software engineering 💻🔥",
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  // twitterX: 'transitive_bs', // If you want to use X.com logo instead
-  // mastodon: '#' // optional mastodon profile URL, provides link verification
-
-  github: 'transitive-bullshit',
-  // gitlab: 'transitive-bullshit', // optional gitlab account
-
-  linkedin: 'fisch2',
+  twitter: 'flakybuild',
+  github: 'flakybuild',
+  linkedin: 'company/flaky-build',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-  // instagram: '#', // optional instagram account
-  // reddit: '#', // optional reddit account
-  // bugtracker: '#', // optional bugtracker URL
+  instagram: 'flaky.build', // optional instagram username
+  // youtube: 'TravelingVanProblem', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  // reddit: 'TravelingVanProblem', // optional reddit username
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -58,18 +52,14 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default',
+  navigationStyle: 'custom',
   // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationLinks: [
+    {
+      title: 'About',
+      url: '/about-us'
+    }
+  ],
 
   footerLinks: [
     {
