ed94f85d852d391da395d54f8cbc5607e02bcdc7
Onni Hakala <onni@flaky.build>
2024-11-08 14:40:46 +0200

    Disable inter regular font in the social image api

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -11,7 +11,6 @@ import {
 } from 'notion-utils'
 
 import * as libConfig from '@/lib/config'
-import interRegularFont from '@/lib/fonts/inter-regular'
 import interSemiBoldFont from '@/lib/fonts/inter-semibold'
 import { mapImageUrl } from '@/lib/map-image-url'
 import { notion } from '@/lib/notion-api'
@@ -158,12 +157,6 @@ export default async function OGImage(
       width: 1200,
       height: 630,
       fonts: [
-        {
-          name: 'Inter',
-          data: interRegularFont,
-          style: 'normal',
-          weight: 400
-        },
         {
           name: 'Inter',
           data: interSemiBoldFont,
